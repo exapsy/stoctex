56347eee994cfc43a162020136d756c4af8a3295
Eksapsy <michael.tolis@gmail.com>
2018-08-04 22:40:54 +0300

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! SceneSelector Fix

--- src/scenes/SceneSelector.js
+++ src/scenes/SceneSelector.js
@@ -39,7 +39,7 @@ export default class SceneSelector extends Component {
   login() {
     this.isLoggedIn()
     .then(value => {
-      this.setState({isLoggedIn: value, isLoading: false})
+      this.setState({isLoggedIn: value, isLoading: value})
     });
   }
 
