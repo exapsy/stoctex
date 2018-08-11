44ab60e492df0b732237c078381854046eb3b2a5
Eksapsy <michael.tolis@gmail.com>
2018-08-11 14:40:18 +0300

    removed unused imports

--- src/scenes/Main/index.js
+++ src/scenes/Main/index.js
@@ -1,11 +1,9 @@
 // APIS & LIBRARIES
 import React, { Component } from 'react';
-import axios                from 'axios';
 
 // LOCAL IMPORTS
 import MainContent  from './MainContent';
 import SideBar        from '../../components/SideBar';
-import rest           from '../../config/rest';
 import './style.scss';
 
 export default class Main extends Component {
