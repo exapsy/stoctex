c8ffb662816f2d5b92b4cf32624c9a84bcb236b5
Eksapsy <michael.tolis@gmail.com>
2018-08-17 14:48:06 +0300

    Beautified Error Boundary

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -20,7 +20,7 @@ export default class ErrorBoundary extends Component {
       hasError: true,
       error: error,
       info: info
-    })
+    });
   }
 
   render() {
@@ -29,16 +29,33 @@ export default class ErrorBoundary extends Component {
       width: '50%',
       margin: 'auto',
       marginTop: '32px',
-      background: 'linearGradient(0, #AA4444, #CC8877)',
+      padding: '64px',
+      backgroundImage: 'linear-gradient(0deg, #1B1B1B, #242124)',
+      borderRadius: '12px'
+    };
+
+    const h1Style = {
+      color: '#FF0800',
+      letterSpacing: '4px'
+    };
+
+    const errorMessageStyle = {
+      padding: '32px',
+      borderRadius: '12px',
+      color: '#F1F1FF',
+      backgroundColor: '#212121',
+      fontFamily: 'monospace'
     };
 
     // If an error was catched then render an Error Block instead of Children
     if(this.state.hasError) {
       return (
         <div className='errorBoundary' style={errorBoundaryStyle}>
-          <h1>Something went wrong</h1>
-          <h3>Error</h3>
-          <p className='errorMessage'>{this.state.error.message}</p>
+          <h1 style={h1Style}>Something went wrong</h1>
+          <h3 style={{color: '#ED2939'}}>Error</h3>
+          <p className='errorMessage' style={errorMessageStyle}>
+            {this.state.error.message}
+          </p>
         </div>
       );
     }
