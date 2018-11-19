170835f6aeec5318ea471ed1a2ca57e35e95a2c5
Eksapsy <michael.tolis@gmail.com>
2018-11-19 17:29:55 +0200

    ListStoreinjection only when user is Logged in

--- src/scenes/Main/index.js
+++ src/scenes/Main/index.js
@@ -5,10 +5,14 @@
 
 // Dependencies
 import React, { Component } from 'react';
-import MainContent  from './MainContent';
-import SideBar        from '../../components/SideBar';
-import { Provider }         from 'mobx-react';
+import MainContent from './MainContent';
+import SideBar from '../../components/SideBar';
+import { Provider } from 'mobx-react';
 import ListStore from '../../stores/ListStore';
+import {
+  observer,
+  inject
+} from 'mobx-react';
 import './style.scss';
 
 /**
@@ -18,10 +22,12 @@ import './style.scss';
  * @class Main
  * @extends {Component}
  */
+@inject('userStore')
+@observer
 export default class Main extends Component {
   render() {
     return (
-      <Provider listStore={new ListStore(ListStore.modes.PRODUCTS)}>
+      <Provider listStore={new ListStore(ListStore.modes.PRODUCTS, this.props.userStore)}>
         <div className='Main'>
           <div className='content'>
             <SideBar/>
